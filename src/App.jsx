de146c61e4d27d8fd77d6b44c8d12ce1bcb475bd
Utkarshsharma1234 <96917045+Utkarshsharma1234@users.noreply.github.com>
2023-07-16 01:36:49 +0530

    client StreamRush

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import {AuthContext} from "../src/context/authContext/AuthContext"
 import { ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+
 const App = () => {
   const {user} = useContext(AuthContext);
   return <Router>
